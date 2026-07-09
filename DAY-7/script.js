0614c38b7b7f60016e1e58231420691b59508930
Rohan Rathodiya <b250503@skit.ac.in>
2026-07-09 22:51:13 +0530

    Add files via upload

--- DAY-7/script.js
+++ DAY-7/script.js
@@ -1,26 +1,9 @@
- let studentName='rohan';
-        let studentBranch='cse';
-        let studentrollno=196;
-        let studentmarks= 90;
-        
-        let students =
-            {
-               name:'rohan',
-                branch:'cse',
-                rollno:196,
-                marks:98,
-                skills:['html','css','js']
-            } 
-            console.log(studentName);
-            console.log(studentBranch);
-            console.log(studentrollno);
-            console.log(studentmarks);
-            console.log(students);
-
-console.log(JSON.stringify(students));
-let st='{"name":"rohan","branch":"cse","rollno":196,"marks":98,"skills":["html","css","js"]}';
-console.log(JSON.parse(st));
-
 fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(json => console.log(json))
+
+      const h="hello rajshree this side";
+      const lowercase =h.toLocaleLowerCase();
+      const trim =h.trim();
+      console.log(lowercase);
+      console.log(trim);  
